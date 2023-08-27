199e661a827bd14baa64f84f35c8464d2864c22e
unknown <anujraghuwanshi2407@gmail.com>
2023-08-27 18:15:33 +0530

    done

---
+++ src/components/SearchBar.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+
+const SearchBar = ({filter , setFilter}) => {
+
+    const [value  , setValue] = useState("")
+    const[print , setPrint] = useState([...filter]);
+
+   function filterArr(e){
+     setValue(e.target.value)
+     let ans = e.target.value
+     setPrint(filter.filter(element => element.includes(ans)));
+     console.log(print)
+    }
+
+
+  return (
+
+
+    <div>
+        <h1>Search items</h1>
+        <input type='text' value={value}  onChange={filterArr} />
+        {
+            print.map(Element=>(
+                <div>
+                    {Element}
+                </div>
+            ))
+        }
+    </div>
+  )
+}
+
+export default SearchBar
